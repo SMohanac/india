89e61b11a788498644bde80392a34f82d17858b7
mohangowdamcmoni@gmail.com <mohana.c@dxc.com>
2020-07-30 21:52:15 +0530

    second commit

--- welcome.java
+++ welcome.java
@@ -4,7 +4,11 @@ public static void main(String[] agrs)
 {
 	System.out.println("Hai welcome to Jenkins");
 	System.out.println("Hai welcome to Jenkins and git");
+	for(int i=0 ;i<10; i++)
+	{
+		System.out.println(i +" - Welcome ");
 
+	}
 
 }
 }
